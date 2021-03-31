6a613842cba8f896f615bc85b6e3f841e2018901
James Cox <james@imaj.es>
2021-03-31 12:14:25 -0500

    Initial gemspec/gemstub

---
+++ lib/smokeclouds/rubocop.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "rubocop/version"
+
+module Smokeclouds
+  module Rubocop
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
